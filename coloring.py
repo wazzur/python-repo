004ad723eb8652f3f655439517f7b85b3c929601
Wazzur <ada10j@my.fsu.edu>
2016-10-16 22:02:01 -0400

    Finished 3D support and improved code in some areas

--- coloring.py
+++ coloring.py
@@ -1,47 +1,41 @@
 import sys
-import math
+import pprint
 from random import randint
 
 
-"TODO: create dictionary to map integers to colors"
-
-
-def createMatrix(X, Y):
-	matrix = [[-1 for x in range(X)] for y in range(Y)]
+def createMatrix(*dim):
+	"Creates a 2D or 3D Matrix"
+	if len(dim) == 2:
+		matrix = [[-1 for x in range(dim[0])] for y in range(dim[1])]
+	elif len(dim) == 3:
+		matrix = [[[-1 for x in range(dim[0])] for y in range(dim[1])] for z in range(dim[2])]
+	else:
+		print("unhandled number of dimensions")
 	return matrix;
 
 
-def printRawMatrix(matrix):
-	for i, y in enumerate(matrix):
-		print(matrix[i])
-	return;
-
-
-def colorMatrix(matrix):
-	"TODO: NEEDS WORK"
-	for i, x in enumerate(matrix):
-		for j, y in enumerate(matrix):
-	                "TODO: set matrix number to dictionary color entry"
+def colorRandomly(matrix, K, dim):
+	"Assigns colors randomly to the matrix based on the given K value in 2 or 3 dimensions"
+	for a, x in enumerate(matrix):
+		for b, y in enumerate(matrix):
+			if dim == 2:
+				matrix[a][b] = randint(0, K-1)
+			elif dim == 3:
+				for c, z in enumerate(matrix):
+					matrix[a][b][c] = randint(0, K-1)
 	return matrix;
 
 
-def colorRandomly(matrix, K):
-	for i, x in enumerate(matrix):
-		for j, y in enumerate(matrix):
-			matrix[i][j] = randint(0, K-1)
-	return matrix;
-
-
-def calculateL(matrix, K):
-	lowest = 99999
+def calculateL(matrix, K, dim):
+	"Calculates the sums of distances of all colors in the matrix and returns the min"
+	lowest = sys.maxsize
 	pairSums = []
 	for c in range(0, K):
-		sum = sumClosestPairs(matrix, c)
+		sum = sumClosestPairs(matrix, c, dim)
 		pairSums.append(sum)
 		if sum < lowest:
 			lowest = sum
 
-	"TODO make pairsums return dictionary representing colors to distances"
 	print("\nThe sums for each colors are: ")
 	print(pairSums)
 
@@ -49,56 +43,77 @@ def calculateL(matrix, K):
 	return lowest;
 
 
-def sumClosestPairs(matrix, color):
+def sumClosestPairs(matrix, color, dim):
 	"find and sum the closest pair distances of same color in matrix"
 	sum = 0
+	for a, x in enumerate(matrix):
+		for b, y in enumerate(matrix):
+			if dim == 2:
+				if matrix[a][b] == color:
+					sum += findClosest(matrix, color, dim, a, b)
+			elif dim == 3:
+				for c, z, in enumerate(matrix):
+					if matrix[a][b][c] == color:
+						sum += findClosest(matrix, color, dim, a, b, c)
+	return sum
 
-	for i, x in enumerate(matrix):
-		for j, y in enumerate(matrix):
-			if matrix[i][j] == color:
-				sum += findClosest(matrix, i, j, color)
-	return sum;
-
-
-def findClosest(matrix, x1, y1, color):
-	min = 99999
-	for i, x2 in enumerate(matrix):
-		for j, y2 in enumerate(matrix):
-			if int(i) == int(x1):
-				if int(j) == int(y1):
+def findClosest(matrix, color, dim, a2, b2, c2=None):
+	"Finds the closest element of the same color in the matrix for both 2D and 3D"
+	min = sys.maxsize
+	for a, x in enumerate(matrix):
+		for b, y in enumerate(matrix):
+			if c2 is None:
+				if int(a) == int(a2) and int(b) == int(b2):
 					continue
-			if matrix[i][j] == color:
-				checkDist = dist(x1,y1,i,j)
-				if checkDist < min:
-					min = checkDist
-	if min == 99999:
-		return 0;
+				if matrix[a][b] == color:
+					checkDist = dist(dim,a2,b2,a,b)
+					if checkDist < min:
+						min = checkDist
+			else:
+				for c, z in enumerate(matrix):
+					if int(a) == int(a2) and int(b) == int(b2) and int(c) == int(c2):
+						continue
+					if matrix[a][b][c] == color:
+						checkDist = dist(dim,a2,b2,c2,a,b,c)
+						if checkDist < min:
+							min = checkDist
+	return [0 if min == sys.maxsize else min][0]
+
+
+def dist(dim, *points):
+	"Calculates the distance between two points using basic algebra"
+	"Supports both 2d and 3d elements"
+	if dim == 2:
+		a = (points[0], points[1])
+		b = (points[2], points[3])
+		return ((((b[0]-a[0])**2)+((b[1]-a[1])**2))**(1/2))
+	elif dim == 3:
+		a = (points[0], points[1], points[2])
+		b = (points[3], points[4], points[5])
+		return ((((b[0]-a[0])**2)+((b[1]-a[1])**2)+((b[2]-a[2])**2))**(1/2))
 	else:
-		return min;
-
-
-def dist(x1, y1, x2, y2):
-	a = (x1, y1)
-	b = (x2, y2)
-	return math.hypot(b[0]-a[0], b[1]-a[1]);
-
+		print("Error in distance function")
 
 def main():
+	"Accepts either command line arguments or will prompt user if none is given"
+	"Calls random coloring and calculates the L for the colored matrix"
+
 	dim = 2
-	if len(sys.argv) == 4 or 5:
+	if len(sys.argv) == 4:
 		X = sys.argv[1]
 		Y = sys.argv[2]
-		if len(sys.argv) == 5:
-			dim = 3
-			Z = sys.argv[3]
-			K = sys.argv[4]
-		else:
-			K = sys.argv[3]
+		K = sys.argv[3]
+	elif len(sys.argv) == 5:
+		dim = 3
+		X = sys.argv[1]
+		Y = sys.argv[2]
+		Z = sys.argv[3]
+		K = sys.argv[4]
 	else:
 		X = input("Enter X dimension value: ")
 		Y = input("Enter Y dimension value: ")
 		choice = input("would you like a Z dimension?  [Y/N]: ")
-		if choice == 'Y':
+		if choice == ('Y' or 'y'):
 			dim = 3
 			Z = input ("Enter Z dimension value: ")
 		K = input("Enter how many colors(K) you would like: ")
@@ -115,17 +130,14 @@ def main():
 		test = createMatrix(int(X), int(Y), int(Z))
 
 	print("\n")
-	test = colorRandomly(test, int(K))
-	printRawMatrix(test)
+	test = colorRandomly(test, int(K), dim)
+	pprint.pprint(test)
 	print("\n")
 
-
-	"colorMatrix(test)"
-	"TODO: test printing colored matrix after function is written"
-
-
-	L = calculateL(test, int(K))
+	L = calculateL(test, int(K), dim)
 	print("\nMin L for this coloring: ")
 	print(L)
 
+
+"RUNS THE PROGRAM"
 main()
